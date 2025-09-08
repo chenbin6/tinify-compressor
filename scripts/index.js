a1722b489bb5e191daafbe17a49162a50b267519
chenbin <smile_chenbin@163.com>
2025-09-08 14:07:18 +0800

    feat: update

--- scripts/index.js
+++ scripts/index.js
@@ -4,25 +4,31 @@ import Compressor from '../lib/compressor.js';
 import getKey from './keys.js';
 import path from 'path';
 
-const dirPath = process.argv[2];
-if (!dirPath) {
+const paths = process.argv.slice(2);
+if (!paths) {
     console.error('Please provide a directory path');
     process.exit(1);
 }
 
-let key = getKey();
-const _tinify = new Compressor(key);
-
-const init = () => _tinify.compressBatch(path.resolve(dirPath));
-
-const _catch = err => {
-    if (err === 'tinify.AccountError') {
-        key = getKey();
-        if (key) {
-            _tinify.updateTinifyKey(key);
-            return init().catch(_catch);
+(async () => {
+    let key = getKey();
+    const _tinify = new Compressor(key);
+    for (const filePath of paths) {
+        const resolvedPath = path.resolve(filePath);
+        let _continue = false;
+        console.log({filePath});
+        while (!_continue && key) {
+            try {
+                await _tinify.compressBatch(resolvedPath);
+                _continue = true;
+            } catch (err) {
+                if (err === 'tinify.AccountError') {
+                    key = getKey();
+                    _continue = !!!key;
+                } else {
+                    _continue = true;
+                }
+            }
         }
     }
-    return Promise.reject(err);
-};
-init().catch(_catch);
+})();
